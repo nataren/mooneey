e9cd993a2df8bb6c3d6f4ad6f0b1fc0cf5c572d0
cesar <cesar@itaca.local>
2011-11-04 19:55:00 -0700

    Initial data model

---
+++ src/db/tables.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE mooneey
+DEFAULT CHARACTER SET = utf8
+DEFAULT COLLATE = utf8_unicode_ci;
+
+CREATE TABLE user (
+user_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_name VARCHAR(255) NOT NULL,
+user_email VARCHAR(255) NOT NULL
+) ENGINE = INNODB;
+
+CREATE TABLE good (
+good_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+good_name VARCHAR(255) NOT NULL,
+good_date TIMESTAMP DEFAULT NOW(),
+good_unit_price INT NOT NULL
+) ENGINE = INNODB;
+
+CREATE TABLE expense (
+expense_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+expense_good_id INTEGER UNSIGNED,
+expense_user_id INTEGER UNSIGNED,
+FOREIGN KEY (expense_good_id) REFERENCES good (good_id),
+FOREIGN KEY (expense_user_id) REFERENCES user (user_id),
+expense_date TIMESTAMP DEFAULT NOW(),
+expense_count INT NOT NULL,
+PRIMARY KEY (expense_id, expense_good_id, expense_user_id)
+) ENGINE = INNODB;
